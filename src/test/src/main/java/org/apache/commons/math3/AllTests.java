6097a03ff71ef80bbe8a5554f43f4d8c31cfa07f
astronautdn <nn_dai@nuaa.edu.cn>
2022-11-22 17:31:56 +0800

    test_in_github

---
+++ src/test/src/main/java/org/apache/commons/math3/AllTests.java
@@ -0,0 +1,9 @@
+package org.apache.commons.math3;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({MATHTestJUnit.class})
+public class AllTests {
+}
